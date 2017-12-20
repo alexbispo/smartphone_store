52d7e755740686c4ba93e3e0238c6f1a2197a262
Daniel Moreto <dfmoreto@gmail.com>
2017-12-20 14:24:46 -0200

    Fragment caching done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220212433) do
+ActiveRecord::Schema.define(version: 20171220114422) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "manufacturer"
