12d4cbb82ddc76c087e1540ca669b80a9d697708
Daniel Moreto <dfmoreto@gmail.com>
2017-12-20 14:24:46 -0200

    Create app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171220212433) do
-
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "manufacturer"
@@ -27,5 +26,4 @@ ActiveRecord::Schema.define(version: 20171220212433) do
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_sells_on_product_id"
   end
-
 end
