5eb1316ec4d11235d6021027713e197c6de79e38
Daniel Moreto <dfmoreto@gmail.com>
2017-12-20 14:24:46 -0200

    Create app

---
+++ test/system/sells_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SellsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit sells_url
+  #
+  #   assert_selector "h1", text: "Sell"
+  # end
+end
